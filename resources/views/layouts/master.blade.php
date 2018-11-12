aa8802ca9976b50af2236735241f1d4f60604476
lcoul <coulombleo@gmail.com>
2018-11-12 15:46:53 +0100

    Salut les amisssssssssssss

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,35 +1,60 @@
-<!doctype html>
-<html lang="fr">
-<head>
-    {!! Html::style('assets/css/bootstrap.css') !!}
-    {!! Html::style('assets/css/bootstrap-theme.css') !!}
-    {!! Html::style('assets/css/bootstrap_flaty.min.css') !!}
-</head>
-<body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-    <a class="navbar-brand" href="#">Ysim</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
 
-    <div class="collapse navbar-collapse" id="navbarColor01">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Acceuil <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Acheter</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Vendre</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Boutique</a>
-            </li>
-        </ul>
+    <!DOCTYPE html>
+    <html lang="fr">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <title>Shop Homepage - Start Bootstrap Template</title>
+
+        <!-- Bootstrap core CSS -->
+        {!! Html::style('assets/css/bootstrap.css') !!}
+        {!! Html::style('assets/css/bootstrap-theme.css') !!}
+        {!! Html::style('assets/css/bootstrap_flaty.min.css') !!}
+
+    </head>
+
+<body>
+
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+        <a class="navbar-brand" href="#">Start </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home
+                        <span class="sr-only">(current)</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Services</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+            </ul>
+        </div>
     </div>
 </nav>
+<div class="container">
 @yield('content')
+</div>
+<footer class="py-5 bg-dark">
+    <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Your Website 2017</p>
+    </div>
+    <!-- /.container -->
+</footer>
 </body>
 </html>
 
